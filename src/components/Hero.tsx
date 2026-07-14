42681f7382b7a327e272f8ed45555e8bab287177
Heman <k.heman0123@gmail.com>
2026-07-14 22:05:24 +0530

    Change image format from JPEG to PNG

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -132,7 +132,7 @@ export default function Hero({
         <div className="hero__portrait-wrapper">
           <div className="hero__portrait-glow" aria-hidden="true" />
           <div className="hero__portrait-card">
-            <img src="/assets/myimage.jpeg" alt="Professional Portrait" className="hero__portrait-img" />
+            <img src="/assets/myimage.png" alt="Professional Portrait" className="hero__portrait-img" />
           </div>
         </div>
       </div>
